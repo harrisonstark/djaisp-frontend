dfb41a68708c0b96f4dffdfdc6ba988b345afb9d
harrisonstark <harrisonstarkshs@gmail.com>
2024-05-17 19:09:37 -0700

    auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import axios from 'axios';
 import App from './App';
 import Redirect from './Redirect';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+axios.defaults.headers.common['HSTOKEN'] = process.env.HSTOKEN;
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Router>
